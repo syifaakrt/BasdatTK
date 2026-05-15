57ca6e01f3c3efa0eec6ae412147138832702448
Syifa Anabella <syifaakrt@gmail.com>
2026-05-15 11:48:42 +0700

    add settings conf

--- tk3/settings.py
+++ tk3/settings.py
@@ -135,6 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 from dotenv import load_dotenv
 import os
